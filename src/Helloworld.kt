c261ea0e01ba4767fe5be5ddb7f822ea96019e48
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2020-02-12 18:40:33 +0530

    HelloWorld Class and function created

---
+++ src/Helloworld.kt
@@ -0,0 +1,9 @@
+class Helloworld {
+    /*fun main(args:Array<String>){
+        println(" Hello this kotlin class")
+    }*/
+
+    fun doPrintHello() {
+        println(" Hello this kotlin class")
+    }
+}
