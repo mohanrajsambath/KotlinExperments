e2d0ba93e3611f998e4accd0901549024ca4fe0f
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2020-02-13 19:37:02 +0530

    Following Things are implemented: 1- Abstract 2- Interface

---
+++ src/FuntionalKotlinBook/Interface/BakeryGood.kt
@@ -0,0 +1,14 @@
+package FuntionalKotlinBook.Interface
+
+abstract class BakeryGood(val flavour: String) {
+    fun eat(): String {
+        return "nom, nom, nom... delicious $flavour ${name()}"
+    }
+
+    fun bake(): String {
+        return "is hot here, isn't??"
+    }
+
+    abstract fun name(): String
+}
+
